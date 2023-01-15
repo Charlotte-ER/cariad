89ff1ab92efe7339d2a6ceef996ce556ec3c6c2e
Charlotte-ER <68346113+Charlotte-ER@users.noreply.github.com>
2023-01-15 12:08:28 +0000

    Create function to parse command line arguments.

---
+++ src/cariad/cariad.py
@@ -0,0 +1,39 @@
+'''Main module. Run cariad from the cli.'''
+
+import argparse
+import os
+import sys
+
+
+def main():
+    ...
+
+
+def parse_cli_args():
+    '''Parse command line arguments, rejecting invalid usage.'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('target', type=str)
+    parser.add_argument('reference', type=str)
+    args, unknown = parser.parse_known_args()
+
+    target = args.target
+    reference = args.reference
+
+    # Handle spaces in target filepath, indicated by unknown arguments
+    if unknown:
+        unknown.insert(0,reference)
+        reference = unknown.pop(-1)
+        unknown.insert(0,target)
+        target = ' '.join(unknown)
+
+    if not os.path.isdir(target):
+        raise sys.exit('Target directory does not exist')
+
+    if not os.path.isfile(os.path.join(target, reference)):
+        raise sys.exit('Reference spreadsheet does not exist in target directory')
+
+    return target, reference
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_cariad.py
@@ -0,0 +1,25 @@
+# Testing: cariad.parse_cli_args()
+def test_no_args():
+    ...
+
+def test_only_one_arg():
+    ...
+
+def test_args_in_wrong_order():
+    ...
+
+
+def test_nonexistent_target_directory():
+    ...
+
+def test_nonexistent_reference_file():
+    ...
+
+def test_target_directory_includes_space():
+    ...
+
+
+
+
+
+
